dd7f87923eddfbadeabacb1d1d67d17700737187
youwei <youweik@163.com>
2020-03-07 22:21:22 +0800

    'config'

---
+++ index.js
@@ -0,0 +1,30 @@
+const rules = require("./rules");
+
+module.exports = {
+  root: true,
+
+  extends: ["standard"],
+
+  env: {
+    browser: true,
+    node: true,
+    amd: false,
+    mocha: false,
+    jasmine: false
+  },
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    }
+  },
+
+  rules: rules
+};
---
+++ rule.js
@@ -0,0 +1,5 @@
+module.exports = {
+  quotes: [2, "single", "avoid-escape"],
+  semi: ["error", "always"],
+  "no-console": "off"
+};
